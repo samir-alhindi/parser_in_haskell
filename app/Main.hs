14d126e12b26525e07f72e8e7783f1ea8aff9663
buzzbuzz20xx <samirskyland@gmail.com>
2025-10-10 12:39:34 +0300

    can now run files from command line arg

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,24 @@ module Main where
 
 import Parsing
 import Evalautor
+import System.Environment (getArgs)
 
 run :: IO ()
 run = do
-    source <- (readFile "C:\\Users\\Samir\\Desktop\\code\\haskell\\expression_parser\\func.smr")
-    case my_parse source of
-        Left err         -> print err
-        Right program'    -> case exec_program program' of
-            Left err -> print err
-            Right io -> io
+    args <- getArgs
+    if length args == 0
+        then helper "C:\\Users\\Samir\\Desktop\\code\\haskell\\expression_parser\\programs\\test3.sfl"
+        else helper (args !! 0)
+
+    where
+        helper :: String -> IO ()
+        helper path = do
+            source <- (readFile path)
+            case my_parse source of
+                Left err         -> print err
+                Right program'    -> case exec_program program' of
+                    Left err -> print err
+                    Right io -> io
 
 main :: IO ()
 main = run
